074db632728a297161bd62d85fa8719e5ac3e558
1Ten Leonardo <leonardo.sousa@1rm0022.cmdo1rm.intra>
2024-01-23 11:55:36 -0300

    Configurado o mkdocs e os arquivos de configuração

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -1,17 +1,31 @@
+NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
+ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
 
-NOTAS = 'C C# d D# E F F# G G# A A# B'.split()
-ESCALAS = {'maior': (0,2,3,5,7,9,11)}
 
-def escalas(tonica, tonalidade):
+def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
     """
-    >>> escalas('C', 'maior')
-    {'notas': ['C', 'D' , 'E' , 'F' , 'G' , 'A' , 'B'], 'graus': ['I' , 'II' , 'III' , 'IV' , 'V' , 'VI' , 'VII']}
+    Gera uma escala apartir de uma tônica e uma tonalidade.
+    
+    Parameters:
+        tonica: Nota que será a tônica da escala
+        tonalidade: Tonialidade da escala
+        
+    Examples:    
+        >>> escalas('C', 'maior')
+        {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+    
+        >>> escalas('A', 'maior')
+        {'notas': ['A' , 'B', 'C#', 'D', 'E', 'F#', 'G#' ], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+        
     """
+    
     intervalos = ESCALAS[tonalidade]
+    tonica_pos = NOTAS.index(tonica)
 
     temp = []
 
     for intervalo in intervalos:
+        nota = (tonica_pos + intervalo) % 12
         temp.append(NOTAS[intervalo])
 
-    return {'notas': temp, 'graus': ['I' , 'II' , 'III' , 'IV' , 'V' , 'VI' , 'VII']}
+    return {'notas': temp, 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
