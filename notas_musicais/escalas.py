f90099346d2e226c2752320fe12e5ba2538da0d1
Leonardo Sousa <leosousa84@gmail.com>
2024-01-23 20:12:37 -0300

    Corrigido o arquivo escalas.py o parametro nota da lista temp

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -2,7 +2,7 @@ NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
 ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
 
 
-def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
+def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
     """
     Gera uma escala apartir de uma tônica e uma tonalidade.
     
@@ -10,12 +10,15 @@ def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
         tonica: Nota que será a tônica da escala
         tonalidade: Tonialidade da escala
         
+    Returns:
+        Um dicionário com as notas da escala e grau.
+
     Examples:    
-        >>> escalas('C', 'maior')
+        >>> escala('C', 'maior')
         {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
     
-        >>> escalas('A', 'maior')
-        {'notas': ['A' , 'B', 'C#', 'D', 'E', 'F#', 'G#' ], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+        >>> escala('A', 'maior')
+        {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
         
     """
     
@@ -26,6 +29,6 @@ def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
 
     for intervalo in intervalos:
         nota = (tonica_pos + intervalo) % 12
-        temp.append(NOTAS[intervalo])
+        temp.append(NOTAS[nota])
 
     return {'notas': temp, 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
