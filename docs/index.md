1a6526639c7c866a1982b56d64b84c3d749810e7
Leonardo Sousa <leosousa84@gmail.com>
2024-01-25 22:14:24 -0300

    Feito o arquivo padrão do CLI e sua documentação

--- docs/index.md
+++ docs/index.md
@@ -1,2 +1,18 @@
 ![logo do projeto](assets/notas.png){ width="300" .center }
 # Notas Musicais
+
+## Como usar?
+Você pode chamar as escalas via linha de comando. Por exemplo:
+```bash
+poetry run escalas
+```
+
+Retornando os graus e as notas correspondentes a essa escala
+```bash
+┏━━━┳━━━━┳━━━━━┳━━━━┳━━━┳━━━━┳━━━━━┓
+┃ I ┃ II ┃ III ┃ IV ┃ V ┃ VI ┃ VII ┃
+┡━━━╇━━━━╇━━━━━╇━━━━╇━━━╇━━━━╇━━━━━┩
+│ F │ G  │ A   │ A# │ C │ D  │ E   │
+└───┴────┴─────┴────┴───┴────┴─────┘
+```
+
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,6 @@
+#! /usr/bin/python3
+
+from pytest import mark
 from typer.testing import CliRunner
 
 from notas_musicais.cli import app
@@ -5,4 +8,10 @@ from notas_musicais.cli import app
 runner = CliRunner()
 
 def test_esacala_cli_deve_retornar_0_ao_stdout():
-    runner.invoke(app)
+    result = runner.invoke(app)
+    assert result.exit_code == 2
+
+@mark.parametrize('nota', ['C', 'D', 'E', 'F', 'G', 'A', 'B'])
+def test_escala_cli_deve_conter_as_notas_na_resposta(nota):
+    result = runner.invoke(app)
+    assert nota in result.stdout
